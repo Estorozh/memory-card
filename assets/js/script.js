94df8e20e2645534a373e2cc7225dbe1e90666ed
Estorozh <mr.ahilis@gmail.com>
2020-02-21 02:18:17 +0300

    show card view

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-let arr=['christenAce','christenTwo','christenThree','christenFour','christenFive','christenSix','christenSeven','christenEight','christenNine','christenTen','christenJack','christenLady','christenKing'], arrSave = [],countCard = arr.length-1;
+let arr=['christenAce','christenTwo','christenThree','christenFour','christenFive','christenSix','christenSeven','christenEight','christenNine','christenTen','christenJack','christenLady','christenKing'], arrSave = [],countCard = arr.length-1, urlCard = 'assets/img/';
 
 function randomInteger(min, max) {
     let rand = min - 0.5 + Math.random() * (max - min + 1);
@@ -26,13 +26,17 @@ function showSaveCard() {
     return arrSave;
 }
 
+function getURLCard(card) {
+    return urlCard+card+'.png';
+}
+
 function control() {
     let num = nextCard();
     if(checkCard(num)) {
         let selectCard = arr[num];
         addSave(num)
         deleteCard(num);
-        return selectCard;
+        return getURLCard(selectCard);
     } else {
         if(arrSave.length<=countCard) {
             return control()
@@ -41,13 +45,21 @@ function control() {
         }
     }
 }
+
 function start(allTime,showTime) {
     function playTime(allTime,showTime) {
-        time = setInterval(()=> {console.log(control()) },showTime*1000);
+        time = setInterval(()=> { document.querySelector('.targetCard').src = control(); },showTime*1000);
         setTimeout(()=> { clearInterval(time) }, allTime*1000);
     }
     
     playTime(allTime,showTime);
 }
 
+// Сделаю пока по умолчанию рубашку
+function defCover(nameCover = 'assets/img/blueCover.png') {
+    document.querySelector('.coverCard').src = nameCover;
+}
+defCover();
+
+// Старт должен отрабатывать на кнопку Старт во вьюхе и принимать два параметра общее время в секундах и время показа в секундах
 start(10,1);
